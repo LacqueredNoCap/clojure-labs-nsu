8ea781bae79f1c09d52000ceec2eb1af894225d5
Lacquered <marshalnsk22854@gmail.com>
2022-11-13 17:06:21 +0700

    Lab 2 [+] Правки по 2.1

--- src/main/clojure/lab2/lab21.clj
+++ src/main/clojure/lab2/lab21.clj
@@ -10,55 +10,49 @@
 
 ; Lab 2.1. Оптимизируйте функцию с помощью мемоизации
 
-; Формулу численного интегрирования от a до b функции f(x) методом трапеций при постоянном шаге dx
-; можно записать как ~ dx/2 * ( f(x0) + 2f(x1) + ... + 2f(xn-1) + f(xn) ), при x0 = a, xn = b
-
-; Длина шага
-(def step 1)
-
-; Вычисление члена суммы
-(defn fxn [f x n]
-  (if (or (= n 0) (>= (* step n) x))
-    (f (* n step))
-    (* 2 (f (* n step)))
-    )
+; Вычисление площади трапеции
+(defn trapezoidArea [f a b]
+  (-> (f a)
+      (+ (f b))
+      (* 0.5)
+      (* (- b a))
+      )
   )
-
-; Мемоизованная версия функции fxn
-(def memoizeFxn (memoize fxn))
 
 ; Вычисление интеграла функции f от 0 до x методом трапеции с постоянным шагом step
-(defn integrate [f]
-  (fn [x]
-    (let [sum (->> (iterate #(+ % step) 0)
-                   (take-while #(<= % x))
-                   (map #(memoizeFxn f x %))
-                   (reduce +)
-                   )
-          ]
-      (* (/ step 2.0) sum)
-      )
+(defn integrate [f step]
+  (let [recurIntegrate (fn [recurIntegrate f x]
+                 (if (> x 0)
+                   (+ (trapezoidArea f (- x step) x) (recurIntegrate recurIntegrate f (- x step)))
+                   0)
+                 )]
+    (partial recurIntegrate recurIntegrate f)
+    ))
+
+; Мемоизованная версия функции integrate
+(defn memoizeIntegrate [f step]
+  (let [recurIntegrate (fn [recurIntegrate x]
+                 (if (> x 0)
+                   (+ (recurIntegrate recurIntegrate (- x step)) (trapezoidArea f (- x step) x))
+                   0)
+                 )
+        ]
+    (partial recurIntegrate (memoize recurIntegrate))
     )
   )
 
-; Мемоизованная версия функции integrate
-(def memoizeIntegrate (memoize integrate))
 
 ; Проверка
 
 ; Тестовые функции
 (def line #(+ (* 2 %) 1))
-(def cube #(* % % %))
-(def shiftedHyperbola #(/ 1 (+ % 1)))
 
-(prn (time ((integrate line) 10))) ; = 110
-(prn (time ((memoizeIntegrate line) 10))) ; = 110
-(prn (time ((memoizeIntegrate line) 10))) ; = 110
+(def nonMemInt (integrate line 1))
+(def memInt (memoizeIntegrate line 1))
 
-(prn (time ((integrate cube) 6))) ; ~ 324
-(prn (time ((memoizeIntegrate cube) 6))) ; ~ 324
-(prn (time ((memoizeIntegrate cube) 6))) ; ~ 324
-
-(prn (time ((integrate shiftedHyperbola) (Math/pow Math/E 5)))) ; ~ 5
-(prn (time ((memoizeIntegrate shiftedHyperbola) (Math/pow Math/E 5)))) ; ~ 5
-(prn (time ((memoizeIntegrate shiftedHyperbola) (Math/pow Math/E 5)))) ; ~ 5
+(prn (time (nonMemInt 100)))
+(prn (time (memInt 100)))
+(prn (time (memInt 100)))
+(prn (time (memInt 101)))
+(prn (time (memInt 102)))
+(prn (time (memInt 99)))
